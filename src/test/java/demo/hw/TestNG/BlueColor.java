530b601d382c1c1804e5737f9218af0d5398b9f3
Agema <zamirbekova.aigerim96@gmail.com>
2024-12-24 15:59:16 +0600

    BlueColor

---
+++ src/test/java/demo/hw/TestNG/BlueColor.java
@@ -0,0 +1,9 @@
+package demo.hw.TestNG;
+
+import com.digital_nomads.Main;
+
+public class BlueColor {
+    public static void main(String[] args) {
+        System.out.println("BlueColor");
+    }
+}
