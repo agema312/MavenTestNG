a6d08734aaa71dac27bd63b63f8f1b81e9612e29
Agema <zamirbekova.aigerim96@gmail.com>
2024-12-25 14:57:32 +0600

    Cit demo class

--- src/test/java/demo/hw/TestNG/BlueColor.java
+++
@@ -1,9 +0,0 @@
-package demo.hw.TestNG;
-
-import com.digital_nomads.Main;
-
-public class BlueColor {
-    public static void main(String[] args) {
-        System.out.println("BlueColor");
-    }
-}
